df928f1da6c02552b113fff90c1b964ff779e374
Blessing Adediwura <blessingadediwura97@gmail.com>
2022-08-22 16:52:42 +0100

    modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+function Greetings() {
+  return <p>Hello there! Good Morning!!</p>;
+}
+
+function UserProfileCard() {
+  return (
+    <div className="card">
+      <img
+        className="class-img"
+        src="https:/www.w3schools.com/howto/img_avatar.png"
+        alt="avatar"
+        style={{ width: 100 }}
+      />
+      <div className="card-name">Blessing Adediwura</div>
+      <div>She is the first Product Manager in her family.</div>
+    </div>
+  );
+}
+
 const App = () => {
   return (
-    <div className="header">
-      <h1>Hello World!</h1>
-    </div>
+    <section className="main-page">
+      <UserProfileCard />
+    </section>
   );
 };
 export default App;
